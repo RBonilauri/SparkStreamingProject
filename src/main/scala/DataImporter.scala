8ca092bf5abb0e3cb017ad43e707f95fbfe3c600
Thomas Tresgots <ttresgots@gmail.com>
2022-07-01 17:17:56 +0200

    1 push man

--- src/main/scala/DataImporter.scala
+++ src/main/scala/DataImporter.scala
@@ -1,11 +1,11 @@
 import org.apache.spark
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 class DataImporter {
 
-  def readCSV(sparkSession: SparkSession, fileName: String): Dataset[Row] = {
+  def readCSV(sparkSession: SparkSession, fileName: String): DataFrame = {
 
-    val csvToRead: Dataset[Row] = sparkSession
+    val csvToRead: DataFrame = sparkSession
       .read
       .option("header", "true")
       .option("inferSchema", "true")
@@ -14,9 +14,8 @@ class DataImporter {
     csvToRead
   }
 
-  def unionCSV(allData: Dataset[Row], newData: Dataset[Row]): Dataset[Row] = {
-
-    val newUnion: Dataset[Row] = allData
+  def unionCSV(allData: DataFrame, newData: DataFrame): DataFrame = {
+    val newUnion: DataFrame = allData
       .unionByName(newData, allowMissingColumns = true)
 
     newUnion
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.{col, to_date, window}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import java.io.File
 
@@ -26,26 +27,81 @@ object Main {
 
     spark.conf.set("spark.sql.shuffle.partitions", "5")
 
-    val arrayConfig = spark.sparkContext.getConf.getAll
+    /*val arrayConfig = spark.sparkContext.getConf.getAll
     println("= SPARK CONFIG =")
     for (conf <- arrayConfig) {
       println(s"\t${conf._1} : ${conf._2}")
-    }
+    }*/
 
-    val data_dir =  System.getProperty("user.dir") + "\\data"
+    val data_dir =  System.getProperty("user.dir") + "\\data\\"
     val data_importer = new DataImporter()
-    val list_of_files = getListOfFiles(data_dir)
-    var BigDF : Dataset[Row] = data_importer.readCSV(spark, list_of_files.head)
 
-//    list_of_files.foreach(_ => {
-//      BigDF = data_importer.unionCSV(BigDF, data_importer.readCSV(spark,_))
-//    })
+    var BigDF : DataFrame = spark.emptyDataFrame
 
-//    BigDF = data_importer.unionCSV(BigDF,BigDF)
-    list_of_files.foreach(
-        _ : String =>{ BigDF  = data_importer
-          .unionCSV(BigDF, data_importer.readCSV(spark, _))
-        })
+    getListOfFiles(data_dir).foreach(file =>
+      BigDF = data_importer.unionCSV(BigDF, data_importer.readCSV(spark,file))
+    )
+    println("All files loaded")
+    val siteUseSchema = BigDF.schema
+
+    val siteUseStream = spark.readStream
+      .schema(siteUseSchema)
+      .option("maxFilesPerTrigger", 1)
+      .format("csv")
+      .option("header", "true")
+      .load(data_dir + "*.csv")
+
+    siteUseStream.createOrReplaceTempView("time_on_data_dot_gouv")
+
+    //      .writeStream
+//      .queryName("Writer")
+//      .foreachBatch((batchDf : DataFrame , batchId : Long) => {
+//        print("looping")
+//        batchDf.show()
+//        if (batchDf.count() >= 100) needsToStop = true
+//      })
+//      .start()
+
+    println("retail is streaming: " + siteUseStream.isStreaming)
+//    while(!needsToStop) {
+//      if(needsToStop) {
+//        retailStream.stop()
+//      }
+//      Thread.sleep(1000)
+//    }
+    val test = siteUseStream.selectExpr("avg_time_on_site" , "nb_actions",  "date")
+      .withColumn("date", to_date(col("date"),"yyyy-MM-dd"))
+
+//  .groupBy(col("nb_actions_per_visit")).sum()
+    test.writeStream.format("memory").queryName("time_on_site").outputMode("append").start()
+
+    val avg_of_avg = siteUseStream
+      .selectExpr(
+        "avg_time_on_site",
+        "date",
       )
+      .groupBy(
+        window(col("date"), "10 days").as("window")
+      )
+      .avg("avg_time_on_site")
+
+    avg_of_avg.writeStream.format("memory").queryName("avg_time").outputMode("complete").start()
+
+
+    for (_ <- 1 to 100) {
+      println("data in time_on_site: " + spark.sql(
+        """
+          |select * from time_on_site
+          |""".stripMargin)
+        .count())
+
+      spark.sql("""
+        |select * from avg_time
+        |order by window
+        |""".stripMargin).show(100, truncate = false)
+      Thread.sleep(5000)
+    }
+    System.in.read()
+    spark.stop()
   }
 }
